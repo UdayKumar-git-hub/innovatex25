105a1466d21db94c46a782c230516884f83672db
UdayKumar-git-hub <udayl4905@gmail.com>
2025-09-16 23:26:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/innovatex25/',   // 👈 important for GitHub Pages
+  base: '/', // ✅ must be root for Vercel
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
