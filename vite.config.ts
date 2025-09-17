c4fecab91c8ed4c79d234937cd5686d91ab0e598
UdayKumar-git-hub <udayl4905@gmail.com>
2025-09-17 22:43:29 +0530

    e

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/',       // important for Vercel root deployment
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
   },
-  server: {
-    fs: {
-      strict: true
-    }
-  }
 });
