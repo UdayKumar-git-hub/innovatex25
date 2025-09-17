84a75d0e3e67f415a5f872018614fb61268a664e
UdayKumar-git-hub <udayl4905@gmail.com>
2025-09-18 00:26:36 +0530

    final

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container missing in index.html');
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>
